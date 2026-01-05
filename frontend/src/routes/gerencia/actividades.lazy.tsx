d00410f7c0dcbcba9708350156ff1c0949a2a32c
Preylin <luxbook.102@gmail.com>
2026-01-05 05:13:26 -0500

    Fix: updated bun image and fixed variable naming3

--- frontend/src/routes/gerencia/actividades.lazy.tsx
+++ frontend/src/routes/gerencia/actividades.lazy.tsx
@@ -1,9 +1,7 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
 import CardAtom from "../../components/atoms/card/Card";
 import UsuarioTable from "../../components/atoms/table/Tabla_panel_control";
-import { ModalAtom } from "../../components/atoms/modal/Modal";
-import React from "react";
-import { Button } from "antd";
+
 
 export const Route = createLazyFileRoute("/gerencia/actividades")({
   component: RouteComponent,
@@ -24,43 +22,3 @@ function RouteComponent() {
   );
 }
 
-
-const ExampleModal: React.FC = () => {
-  const [open, setOpen] = React.useState<boolean>(false);
-  const [loading, setLoading] = React.useState<boolean>(true);
-
-  const showLoading = () => {
-    setOpen(true);
-    setLoading(true);
-
-    // Simple loading mock. You should add cleanup logic in real world.
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-  };
-
-  return (
-    <>
-      <Button type="primary" onClick={showLoading}>
-        Open Modal
-      </Button>
-      <ModalAtom
-        title={<p className="text-red-300">Loading Modal</p>}
-        footer={
-          <Button type="primary" onClick={showLoading}>
-            Reload
-          </Button>
-        }
-        loading={loading}
-        open={open}
-        onClose={() => setOpen(false)}
-      >
-        <CardAtom title={"Usuarios"} extra={"Ver más"} variant="borderless">
-        <div className="w-100 overflow-hidden">
-          <UsuarioTable />
-        </div>
-      </CardAtom>
-      </ModalAtom>
-    </>
-  );
-};
